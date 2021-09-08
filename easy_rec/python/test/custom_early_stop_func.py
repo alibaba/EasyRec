03ff9afb659d19fce60e542a65c16a8c9da61e1a
杨熙 <mengli.cml@alibaba-inc.com>
2021-09-08 11:18:46 +0800

    add missing file

--- easy_rec/python/test/custom_early_stop_func.py
+++ easy_rec/python/test/custom_early_stop_func.py
@@ -1,10 +1,9 @@
 # -*- encoding:utf-8 -*-
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
-import logging
-import os
-import numpy as np
 import json
+import logging
+
 
 def custom_early_stop_func(eval_results, func_param):
   params = json.loads(func_param)
@@ -13,6 +12,8 @@ def custom_early_stop_func(eval_results, func_param):
   for step, metrics in eval_results.items():
     val = metrics[metric_name]
     if val > tmp_thre:
-      logging.info('At step %s, metric "%s" has value %s, which is larger than %s, causing early stop.' % (step, metric_name, val, tmp_thre))
+      logging.info(
+          'At step %s, metric "%s" has value %s, which is larger than %s, causing early stop.'
+          % (step, metric_name, val, tmp_thre))
       return True
-  return False 
+  return False
