1d115fd03a06fb928248c43b5c749e3ac5dd868c
yancheng.lgq <yancheng.lgq@alibaba-inc.com>
2021-10-21 20:52:43 +0800

    add support for sequence feature

--- easy_rec/python/layers/seq_input_layer.py
+++ easy_rec/python/layers/seq_input_layer.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 from easy_rec.python.compat.feature_column import feature_column
 from easy_rec.python.feature_column.feature_column import FeatureColumnParser
 from easy_rec.python.protos.feature_config_pb2 import WideOrDeep
-
+import logging
 if tf.__version__ >= '2.0':
   tf = tf.compat.v1
 
@@ -22,43 +22,54 @@ class SeqInputLayer(object):
 
   def __call__(self, features, group_name):
     feature_column_dict = self._fc_parser.deep_columns
+    feature_column_dict.update(self._fc_parser.sequence_columns)
 
     builder = feature_column._LazyBuilder(features)
-    key_features = []
-    hist_emb_features = []
-    hist_seqlen_features = []
+
     feature_dict = self._feature_groups_config[group_name]
+    seq_att_map = feature_dict.seq_att_map
+    keys = seq_att_map.key
+    hist_seqs = seq_att_map.hist_seq
 
-    for x in feature_dict.seq_att_map:
-      with tf.variable_scope('emd_' + x.key, reuse=tf.AUTO_REUSE):
-        key_tensor = feature_column_dict[x.key]._get_dense_tensor(builder)
-      with tf.variable_scope('hist_emd_' + x.hist_seq, reuse=tf.AUTO_REUSE):
-        hist_embedding, hist_seq_len = feature_column_dict[
-            x.hist_seq]._get_sequence_dense_tensor(builder)
+    def _seq_embed_name(input_name):
+      input_name = input_name.split(':')[0]
+      input_name = input_name.split('/')[:2]
+      return 'sequence_feature/' + '/'.join(input_name)
 
-      key_features.append(key_tensor)
-      hist_emb_features.append(hist_embedding)
-      hist_seqlen_features.append(hist_seq_len)
+    with tf.variable_scope(group_name, reuse=tf.AUTO_REUSE):
+      key_tensors = []
+      for key in keys:
+        qfc = feature_column_dict[key]
+        with tf.variable_scope(qfc._var_scope_name):
+          key_tensors.append(
+              feature_column_dict[key]._get_dense_tensor(builder))
+      for key_tensor in key_tensors:
+        tf.summary.histogram(_seq_embed_name(key_tensor.name), key_tensor)
 
-      # din_features.append({'key': key_tensor, 'hist_seq_emb': hist_embedding, 'hist_seq_len': hist_seq_len})
-    if len(feature_dict.seq_att_map) > 1:
+      hist_tensors = []
+      for hist_seq in hist_seqs:
+        seq_fc = feature_column_dict[hist_seq]
+        with tf.variable_scope(seq_fc._var_scope_name):
+          hist_tensors.append(
+              feature_column_dict[hist_seq]._get_sequence_dense_tensor(builder))
+      
+      for hist_embed, hist_seq_len in hist_tensors:
+        tf.summary.histogram(_seq_embed_name(hist_embed.name), hist_embed)
+        tf.summary.histogram(_seq_embed_name(hist_seq_len.name), hist_seq_len)
+
       features = {
-          'key': tf.concat(key_features, axis=1),
-          'hist_seq_emb': tf.concat(hist_emb_features, axis=2),
-          'hist_seq_len': hist_seqlen_features[0]
-      }
-    else:
-      features = {
-          'key': key_features[0],
-          'hist_seq_emb': hist_emb_features[0],
-          'hist_seq_len': hist_seqlen_features[0]
+        'key': tf.concat(key_tensors, axis=-1),
+        'hist_seq_emb': tf.concat([x[0] for x in hist_tensors], axis=-1),
+        'hist_seq_len': hist_tensors[0][1]
       }
     return features
 
   def get_wide_deep_dict(self):
     wide_and_deep_dict = {}
     for group_name_config in self._feature_groups_config.values():
-      for x in group_name_config.seq_att_map:
-        wide_and_deep_dict[x.key] = WideOrDeep.DEEP
-        wide_and_deep_dict[x.hist_seq] = WideOrDeep.DEEP
+      seq_att = group_name_config.seq_att_map
+      for key in seq_att.key:
+        wide_and_deep_dict[key] = WideOrDeep.DEEP
+      for hist_seq in seq_att.hist_seq:
+        wide_and_deep_dict[hist_seq] = WideOrDeep.DEEP
     return wide_and_deep_dict
