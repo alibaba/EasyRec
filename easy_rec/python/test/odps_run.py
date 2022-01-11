3bd3f50df36c20eb0c34c25d6b1048ab4c47c303
杨熙 <mengli.cml@alibaba-inc.com>
2022-01-11 11:23:17 +0800

    add odps test

--- easy_rec/python/test/odps_run.py
+++ easy_rec/python/test/odps_run.py
@@ -187,7 +187,7 @@ class TestPipelineOnOdps(tf.test.TestCase):
         'boundary/create_external_boundary_table.sql',
         'boundary/create_inner_boundary_table.sql',
     ]
-    test_files = ['boundary/train_compat.sql']
+    test_files = ['boundary/train_multi_tower_model.sql', 'boundary/train_compat.sql']
     end_file = ['boundary/drop_table.sql']
     tot = OdpsTest(start_files, test_files, end_file, odps_oss_config)
     tot.start_test()
