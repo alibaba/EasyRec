e3551e212df1c2afd8ed07468d6e7e35343b2bfd
天邑 <hongsheng.jhs@alibaba-inc.com>
2022-03-21 15:03:53 +0800

    merge master

--- easy_rec/python/test/run.py
+++ easy_rec/python/test/run.py
@@ -2,10 +2,13 @@
 # -*- encoding:utf-8 -*-
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
+import logging
 import os
 import unittest
 
 import tensorflow as tf
+
+from easy_rec.python.utils import test_utils
 
 if tf.__version__ >= '2.0':
   tf = tf.compat.v1
@@ -15,55 +18,97 @@ tf.app.flags.DEFINE_string('list_test_to_file', None, 'list all tests')
 tf.app.flags.DEFINE_string('pattern', '*_test.py', 'test file pattern')
 tf.app.flags.DEFINE_string('test_dir', 'easy_rec/python/test',
                            'directory to be tested')
+tf.app.flags.DEFINE_integer('num_parallel', 10,
+                            'number of parallel executed cases.')
+tf.app.flags.DEFINE_integer('timeout', 3600,
+                            'maximal execute time in seconds for each case.')
 FLAGS = tf.flags.FLAGS
 
 
 def gather_test_cases(test_dir, pattern):
-  test_suite = unittest.TestSuite()
   discover = unittest.defaultTestLoader.discover(
       test_dir, pattern=pattern, top_level_dir=None)
   all_tests = []
   for suite_discovered in discover:
-
     for test_case in suite_discovered:
-      test_suite.addTest(test_case)
       if hasattr(test_case, '__iter__'):
         for subcase in test_case:
-          if FLAGS.list_tests or FLAGS.list_test_to_file:
-            print(subcase.id())
-            tid = subcase.id().split('.')[0]
-            if tid not in all_tests:
-              all_tests.append(tid)
+          toks = subcase.id().split('.')
+          case_file = toks[0]
+          case_name = '.'.join(toks[1:])
+          if (case_file, case_name) not in all_tests:
+            all_tests.append((case_file, case_name))
       else:
-        if FLAGS.list_tests or FLAGS.list_test_to_file:
-          print(test_case.id())
-          tid = subcase.id().split('.')[0]
-          if tid not in all_tests:
-            all_tests.append(tid)
+        toks = subcase.id().split('.')[0]
+        case_file = toks[0]
+        case_name = '.'.join(toks[1:])
+        if (case_file, case_name) not in all_tests:
+          all_tests.append((case_file, case_name))
   if FLAGS.list_test_to_file:
-    print('save test lists to %s' % FLAGS.list_test_to_file)
+    logging.info('Total number of cases: %d' % len(all_tests))
+    logging.info('save test lists to %s' % FLAGS.list_test_to_file)
     with open(FLAGS.list_test_to_file, 'w') as fout:
-      for t_name in all_tests:
-        fout.write('%s\n' % t_name)
-  return test_suite
+      for t_file, t_name in all_tests:
+        fout.write('%s %s\n' % (t_file, t_name))
+  elif FLAGS.list_tests:
+    logging.info('Total number of cases: %d' % len(all_tests))
+    for t_file, t_name in all_tests:
+      logging.info('\t%s.%s' % (t_file, t_name))
+  return all_tests
 
 
 def main(argv):
-  runner = unittest.TextTestRunner()
-  test_suite = gather_test_cases(os.path.abspath(FLAGS.test_dir), FLAGS.pattern)
+  all_tests = gather_test_cases(os.path.abspath(FLAGS.test_dir), FLAGS.pattern)
   if FLAGS.list_tests or FLAGS.list_test_to_file:
     return
 
-  result = runner.run(test_suite)
-  if not result.wasSuccessful():
-    print('FailNum: %d ErrorNum: %d' %
-          (len(result.failures), len(result.errors)))
+  test_dir = os.environ.get('TEST_DIR', '.')
+  if not os.path.isdir(test_dir):
+    os.makedirs(test_dir)
+  test_log_dir = os.path.join(test_dir, 'logs')
+  os.makedirs(test_log_dir)
+  logging.info('Total number of cases: %d test_dir: %s' %
+               (len(all_tests), test_dir))
+  procs = {}
+  failed_cases = []
+  for case_file, case_name in all_tests:
+    while len(procs) >= FLAGS.num_parallel:
+      procs_done = []
+      for proc in procs:
+        if proc.poll() is not None:
+          if proc.returncode != 0:
+            fail_file, fail_name = procs[proc]
+            failed_cases.append((fail_file, fail_name, proc.returncode))
+          procs_done.append(proc)
+      for proc in procs_done:
+        del procs[proc]
+    cmd = 'python -m easy_rec.python.test.%s %s' % (case_file, case_name)
+    log_file = '%s/%s.%s.log' % (test_log_dir, case_file, case_name)
+    logging.info('Run %s.%s Log: %s' % (case_file, case_name, log_file))
+    proc = test_utils.run_cmd(cmd, log_file)
+    procs[proc] = (case_file, case_name)
+
+  for proc in procs:
+    try:
+      proc.wait()
+    except Exception as ex:
+      fail_file, fail_name = procs[proc]
+      logging.info('Case Exception: %s.%s %s' % (fail_file, fail_name, str(ex)))
+      proc.kill()
+
+    if proc.returncode != 0:
+      fail_file, fail_name = procs[proc]
+      failed_cases.append((fail_file, fail_name, proc.returncode))
+
+  if len(failed_cases) > 0:
+    logging.info('Number Cases Failed: %d' % len(failed_cases))
+    for fail_file, fail_name, exit_code in failed_cases:
+      logging.info('\t%s.%s failed, exit_code:%d log: %s.%s.log' %
+                   (fail_file, fail_name, exit_code, fail_file, fail_name))
+    return 1
   else:
-    if 'UnitTestSucceedFlag' in os.environ:
-      flag_file = os.environ['UnitTestSucceedFlag']
-      with open(flag_file, 'w') as fout:
-        fout.write('unit succeed.')
-      print('create flag file: %s' % flag_file)
+    logging.info('TestSucceed.')
+    return 0
 
 
 if __name__ == '__main__':
--- easy_rec/python/utils/test_utils.py
+++ easy_rec/python/utils/test_utils.py
@@ -74,10 +74,11 @@ def get_available_gpus():
 
 def run_cmd(cmd_str, log_file):
   """Run a shell cmd."""
-  logging.info('%s > %s 2>&1 ' % (cmd_str, log_file))
+  cmd_str = cmd_str.replace('\r', ' ').replace('\n', ' ')
+  logging.info('RUNCMD: %s > %s 2>&1 ' % (cmd_str, log_file))
   with open(log_file, 'w') as lfile:
     return subprocess.Popen(
-        cmd_str.split(), stdout=lfile, stderr=subprocess.STDOUT)
+        cmd_str, stdout=lfile, stderr=subprocess.STDOUT, shell=True)
 
 
 def RunAsSubprocess(f):
