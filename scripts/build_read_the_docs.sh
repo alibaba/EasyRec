0413e16ce8aa63d7650604723aa7c15f66474be1
杨熙 <mengli.cml@alibaba-inc.com>
2022-03-30 14:22:21 +0800

    not install docs

--- scripts/build_read_the_docs.sh
+++ scripts/build_read_the_docs.sh
@@ -7,7 +7,7 @@ bash scripts/gen_proto.sh
 PATH=./protoc/bin/ protoc/bin/protoc --doc_out=html,proto.html:docs/source easy_rec/python/protos/*.proto
 sed -i 's#<p>#<pre>#g;s#</p>#</pre>#g' docs/source/proto.html
 
-# pip3 install -r requirements/docs.txt
-# pip install -r requirements/docs.txt
+pip3 install -r requirements/docs.txt
+pip install -r requirements/docs.txt
 pip3 install tensorflow==2.3
 pip install tensorflow==2.3
