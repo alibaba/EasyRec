f94eabbb3bf9de7628023ea826c8f49936a5b904
yancheng.lgq <yancheng.lgq@alibaba-inc.com>
2022-05-27 16:36:19 +0800

    add support easyrec distribute metrics

--- easy_rec/python/core/easyrec_metrics/__init__.py
+++ easy_rec/python/core/easyrec_metrics/__init__.py
@@ -3,10 +3,6 @@ import os
 
 import tensorflow as tf
 
-from easy_rec.python.builders import loss_builder
-from easy_rec.python.core import metrics as metrics_lib
-from easy_rec.python.model.easy_rec_model import EasyRecModel
-from easy_rec.python.protos.loss_pb2 import LossType
 from easy_rec.python.utils import pai_util
 
 distribute_eval = os.environ.get('distribute_eval')
