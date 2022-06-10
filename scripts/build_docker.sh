47d820cf6a80c26d7a3755cc424b7fde67aa05d6
chengmengli06 <chengmengli06@qq.com>
2022-06-10 11:28:39 +0800

    update build docker script

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+bash scripts/gen_proto.sh
+if [ $? -ne 0 ]
+then
+  echo "gen proto failed"
+  exit 1
+fi
+
 sudo docker build --net=host . -f docker/Dockerfile -t  datascience-registry.cn-beijing.cr.aliyuncs.com/easyrec/easyrec:py36-tf1.15
