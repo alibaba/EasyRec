e347ff47edfc7c62257fb30f5abf661dedd4502d
chengmengli06 <chengmengli06@qq.com>
2022-06-10 13:40:27 +0800

    fix according to comments

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -7,4 +7,8 @@ then
   exit 1
 fi
 
-sudo docker build --net=host . -f docker/Dockerfile -t  datascience-registry.cn-beijing.cr.aliyuncs.com/easyrec/easyrec:py36-tf1.15
+export PYTHONPATH=.
+version=`python -c "import easy_rec; print(easy_rec.__version__)" | tail -1`
+echo "EasyRec Version: $version"
+
+sudo docker build --net=host . -f docker/Dockerfile -t  datascience-registry.cn-beijing.cr.aliyuncs.com/easyrec/easyrec:py36-tf1.15-${version}
