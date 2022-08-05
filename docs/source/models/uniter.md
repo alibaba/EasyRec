9402ae63d885a2f0bf328e3f630ab3c9c0d73a83
weisu <weisu.yxd@alibaba-inc.com>
2022-08-05 23:53:48 +0800

    [feats]: add Uniter model and Uniter layer for DBMTL model

---
+++ docs/source/models/uniter.md
@@ -0,0 +1,129 @@
+# UNITER
+
+### 简介
+
+人类感知和处理周围世界的信息是通过多种手段综合起来的，比如视觉、触觉和听觉等。
+在分析处理外部信息时，人脑都是综合多种形式的信息进行补充和比对进而生成决策判断结果，很少只单独用一个单一形式的信息。
+这种信息的不同发生的方式叫做模态（Modality）。
+
+推荐系统中的物品很多情况下天然包含了各种模态的信息，比如物品的文本标题，物品的主副图，物品的属性信息等。
+各个模态的信息相互补充构成了人对物品的完整理解。任何单一一个模态的信息，都不足以完整描述一件物品。
+
+基于以上分析，多模态物品表征学习成为一种趋势。
+
+UNITER (UNiversal Image-TExt Representation Learning) 模型是基于Transformer结构来融合图像和文本的跨模态信息，
+其结构如下图所示：
+
+![uniter](../../images/models/uniter.png)
+
+为了使UNITER模型更加适用于推荐场景，我们对其进行了一下扩展，包括加入物品的属性特征，与图像、文本的信息进行融合，
+同时还支持融合后的跨模态特征与其他的特征（如各类统计型特征）拼接后再过MLP层。
+
+模型支持四种类型的特征组（`feature group`），如下所述。
+不一定需要有全部四种类型的输入特征，只需要保证至少有一种类型的输入特征即可训练模型。根据输入特征类型的不同，部分网络结构可能会被`短路`（skip）掉。
+
+视觉特征提取模块通常是一个CNN-based的模型，用来提取图像或视频特征，以便后续接入transformer模块。
+
+文本型特征包括两部分：
+
+- 常规类型的特征，包括数值特征、单值类别特征、多值类别特征；（对应配置名为`general`的`feature group`）
+- 不定长文本序列特征，例如 物品的`title`、`description`等；（对应配置名为`text`的`feature group`）
+  每个特征需要转换为相同维度的embedding，以便接入后续的transformer模块。
+
+其他特征：不参与`单模态学习模块`和`跨模态融合模块`的输入特征，直接与`跨模态融合模块`的结果拼接后，接入后续的MLP，通常是一些统计类特征。
+（对应配置名为`other`的`feature group`）
+
+UNITER模型在多目标学习任务重的应用请参考 [DBMTL+UNITER](dbmtl.md)。
+
+### 配置说明
+
+```protobuf
+model_config: {
+  model_class: 'Uniter'
+  feature_groups: {
+    group_name: 'image'
+    feature_names: 'embedding'
+    wide_deep: DEEP
+  }
+  feature_groups: {
+    group_name: 'general'
+    feature_names: 'user_id'
+    feature_names: 'movie_id'
+    feature_names: 'gender'
+    feature_names: 'age'
+    feature_names: 'occupation'
+    feature_names: 'zip_id'
+    feature_names: 'movie_year_bin'
+    wide_deep: DEEP
+  }
+  feature_groups: {
+    group_name: 'text'
+    feature_names: 'title'
+    feature_names: 'genres'
+    wide_deep: DEEP
+  }
+  feature_groups: {
+    group_name: 'other'
+    feature_names: 'score_year_diff'
+    feature_names: 'score_time'
+    wide_deep: DEEP
+  }
+  uniter {
+    config {
+      hidden_size: 512
+      num_attention_heads: 4
+      num_hidden_layers: 2
+      intermediate_size: 512
+      hidden_act: 'swish'
+      max_position_embeddings: 16
+      hidden_dropout_prob: 0.1
+      attention_probs_dropout_prob: 0
+      other_feature_dnn: {
+        hidden_units: 256
+        hidden_units: 128
+      }
+    }
+    final_dnn: {
+        hidden_units: 256
+        hidden_units: 64
+    }
+  }
+  embedding_regularization: 1e-6
+}
+```
+
+- model_class: 'Uniter', 不需要修改
+
+- feature_groups:
+
+  - 配置一个名为`image`的feature_group，包含一个图像特征，或者一组embedding_size相同的图像特征（对应视频的多个帧，或者图像的多个region）。
+  - 配置一个名为`general`的feature_group，包含需要做跨模态attention的常规特征，这些特征的`embedding_dim`必须相同。
+  - 配置一个名为`text`的feature_group，包含需要做跨模态attention的不定长文本序列特征，这些特征的`embedding_dim`必须相同。
+  - 注意：CMBF 模型要求所有文本侧（包括`text`和`general`两个特征组）输入特征的 embedding_dim 保持一致。
+  - \[可选\] 配置一个名为`other`的feature_group，包含不需要做跨模态attention的其他特征，如各类统计特征。
+
+- uniter/config: UNITER 模型相关的参数
+
+  - num_attention_heads: 跨模态融合模块中的 head 数量
+  - hidden_size: transformer模块的隐层单元数，必须是`num_attention_heads`的整数倍。
+  - num_hidden_layers: transformer模块中的self-attention层数，建议设在1到5之间，默认为1
+  - intermediate_size: transformer模块中MLP层的神经元个数
+  - hidden_act: transformer模块中MLP层的激活函数，目前支持：'relu'、'swish'、'gelu'、'tanh'
+  - attention_probs_dropout_prob: self/cross attention模块attention权重的dropout概率
+  - hidden_dropout_prob: multi-headed attention模块中FC layer的dropout概率
+  - use_position_embeddings: bool, default is true；是否为文本序列添加位置编码
+  - max_position_embeddings: 文本序列的最大位置，当`use_position_embeddings`为true时，必须配置；并且必须大于或等于所有特征配置`max_seq_len`的最大值
+  - other_feature_dnn: \[可选\] 其他特征的MLP网络配置
+
+- uniter/final_dnn: 输出模块的MLP网络配置
+
+- embedding_regularization: 对embedding部分加regularization，防止overfit
+
+### 示例Config
+
+[Uniter_demo.config](https://github.com/alibaba/EasyRec/blob/master/samples/model_config/uniter_on_movielens.config)
+
+### 参考论文
+
+[UNITER: UNiversal Image-TExt Representation Learning](https://arxiv.org/pdf/1909.11740.pdf)
+
---
+++ easy_rec/python/layers/uniter.py
@@ -0,0 +1,279 @@
+# -*- encoding:utf-8 -*-
+# Copyright (c) Alibaba, Inc. and its affiliates.
+import tensorflow as tf
+
+from easy_rec.python.layers import dnn
+from easy_rec.python.layers import multihead_cross_attention
+from easy_rec.python.utils.shape_utils import get_shape_list
+
+if tf.__version__ >= '2.0':
+  tf = tf.compat.v
+
+
+class Uniter(object):
+  """UNITER: UNiversal Image-TExt Representation Learning
+
+    paper: https://arxiv.org/abs/1909.11740
+  """
+  def __init__(self, model_config, feature_configs, features, uniter_config,
+               input_layer):
+    self._model_config = uniter_config
+    tower_num = 0
+    self._img_features = None
+    if input_layer.has_group('image'):
+      self._img_features, _ = input_layer(features, 'image')
+      tower_num += 1
+    self._general_features = None
+    if input_layer.has_group('general'):
+      self._general_features, _ = input_layer(features, 'general')
+      tower_num += 1
+    self._txt_seq_features = None
+    if input_layer.has_group('text'):
+      self._txt_seq_features = input_layer(features, 'text', is_combine=False)
+      tower_num += 1
+    self._use_token_type = True if tower_num > 1 else False
+    self._other_features = None
+    if input_layer.has_group('other'):  # e.g. statistical feature
+      self._other_features, _ = input_layer(features, 'other')
+      tower_num += 1
+    assert tower_num > 0, 'there must be one of the feature groups: [image, text, general, other]'
+
+    self._general_feature_num = 0
+    self._txt_feature_num, self._img_feature_num = 0, 0
+    general_feature_names = set()
+    img_feature_names, txt_feature_names = set(), set()
+    for fea_group in model_config.feature_groups:
+      if fea_group.group_name == 'general':
+        self._general_feature_num = len(fea_group.feature_names)
+        general_feature_names = set(fea_group.feature_names)
+        assert self._general_feature_num == len(general_feature_names), (
+            'there are duplicate features in `general` feature group')
+      elif fea_group.group_name == 'image':
+        self._img_feature_num = len(fea_group.feature_names)
+        img_feature_names = set(fea_group.feature_names)
+        assert self._img_feature_num == len(img_feature_names), (
+            'there are duplicate features in `image` feature group')
+      elif fea_group.group_name == 'text':
+        self._txt_feature_num = len(fea_group.feature_names)
+        txt_feature_names = set(fea_group.feature_names)
+        assert self._txt_feature_num == len(txt_feature_names), (
+          'there are duplicate features in `text` feature group')
+
+    if self._txt_feature_num > 1 or self._img_feature_num > 1:
+      self._use_token_type = True
+    self._token_type_vocab_size = self._txt_feature_num
+    if self._img_feature_num > 0:
+      self._token_type_vocab_size += 1
+    if self._general_feature_num > 0:
+      self._token_type_vocab_size += 1
+
+    max_seq_len = 0
+    txt_fea_emb_dim_list = []
+    general_emb_dim_list = []
+    img_fea_emb_dim_list = []
+    for feature_config in feature_configs:
+      fea_name = feature_config.input_names[0]
+      if feature_config.HasField('feature_name'):
+        fea_name = feature_config.feature_name
+      if fea_name in img_feature_names:
+        img_fea_emb_dim_list.append(feature_config.raw_input_dim)
+      if fea_name in general_feature_names:
+        general_emb_dim_list.append(feature_config.embedding_dim)
+      if fea_name in txt_feature_names:
+        txt_fea_emb_dim_list.append(feature_config.embedding_dim)
+        if feature_config.HasField('max_seq_len'):
+          assert feature_config.max_seq_len > 0, (
+              'feature config `max_seq_len` must be greater than 0 for feature: '
+              + fea_name)
+          if feature_config.max_seq_len > max_seq_len:
+            max_seq_len = feature_config.max_seq_len
+
+    unique_dim_num = len(set(txt_fea_emb_dim_list))
+    assert unique_dim_num <= 1 and len(txt_fea_emb_dim_list) == self._txt_feature_num, (
+        'Uniter requires that all `text` feature dimensions must be consistent.'
+    )
+    unique_dim_num = len(set(img_fea_emb_dim_list))
+    assert unique_dim_num <= 1 and len(img_fea_emb_dim_list) == self._img_feature_num, (
+        'Uniter requires that all `image` feature dimensions must be consistent.')
+    unique_dim_num = len(set(general_emb_dim_list))
+    assert unique_dim_num <= 1 and len(general_emb_dim_list) == self._general_feature_num, (
+        'Uniter requires that all `general` feature dimensions must be consistent.')
+
+    if self._txt_feature_num > 0 and uniter_config.use_position_embeddings:
+      assert uniter_config.max_position_embeddings > 0, (
+          'model config `max_position_embeddings` must be greater than 0. ')
+      assert uniter_config.max_position_embeddings >= max_seq_len, (
+          'model config `max_position_embeddings` must be greater than or equal to the maximum of all feature config '
+          '`max_seq_len`, which is %d' % max_seq_len)
+
+    self._img_emb_size = img_fea_emb_dim_list[0] if img_fea_emb_dim_list else 0
+    self._txt_emb_size = txt_fea_emb_dim_list[0] if txt_fea_emb_dim_list else 0
+    self._general_emb_size = general_emb_dim_list[0] if general_emb_dim_list else 0
+    if self._img_features is not None:
+      assert self._img_emb_size > 0, '`image` feature dimensions must be greater than 0, set by `raw_input_dim`'
+
+  def text_embeddings(self, token_type_id):
+    all_txt_features = []
+    input_masks = []
+    hidden_size = self._model_config.hidden_size
+    if self._general_features is not None:
+      general_features = self._general_features
+      if self._general_emb_size != hidden_size:
+        # Run a linear projection of `hidden_size`
+        general_features = tf.reshape(
+          general_features, shape=[-1, self._general_emb_size])
+        general_features = tf.layers.dense(
+          general_features, hidden_size, name='txt_projection')
+      general_features = tf.reshape(
+        general_features, shape=[-1, self._general_feature_num, hidden_size])
+
+      batch_size = tf.shape(general_features)[0]
+      general_features = multihead_cross_attention.embedding_postprocessor(
+        general_features,
+        use_token_type=self._use_token_type,
+        token_type_ids=tf.ones(
+          shape=tf.stack([batch_size, self._general_feature_num]), dtype=tf.int32) * token_type_id,
+        token_type_vocab_size=self._token_type_vocab_size,
+        reuse_token_type=tf.AUTO_REUSE,
+        use_position_embeddings=False,
+        dropout_prob=self._model_config.hidden_dropout_prob)
+
+      all_txt_features.append(general_features)
+      mask = tf.ones(
+        shape=tf.stack([batch_size, self._general_feature_num]),
+        dtype=tf.int32)
+      input_masks.append(mask)
+
+    if self._txt_seq_features is not None:
+      def dynamic_mask(x, max_len):
+        ones = tf.ones(shape=tf.stack([x]), dtype=tf.int32)
+        zeros = tf.zeros(shape=tf.stack([max_len - x]), dtype=tf.int32)
+        return tf.concat([ones, zeros], axis=0)
+
+      token_type_id += len(all_txt_features)
+      for i, (seq_fea, seq_len) in enumerate(self._txt_seq_features):
+        batch_size, max_seq_len, emb_size = get_shape_list(seq_fea, 3)
+        if emb_size != hidden_size:
+          seq_fea = tf.reshape(seq_fea, shape=[-1, emb_size])
+          seq_fea = tf.layers.dense(
+            seq_fea, hidden_size, name='txt_seq_projection_%d' % i)
+          seq_fea = tf.reshape(seq_fea, shape=[-1, max_seq_len, hidden_size])
+
+        seq_fea = multihead_cross_attention.embedding_postprocessor(
+          seq_fea,
+          use_token_type=self._use_token_type,
+          token_type_ids=tf.ones(
+            shape=tf.stack([batch_size, max_seq_len]), dtype=tf.int32) * (i + token_type_id),
+          token_type_vocab_size=self._token_type_vocab_size,
+          reuse_token_type=tf.AUTO_REUSE,
+          use_position_embeddings=self._model_config.use_position_embeddings,
+          max_position_embeddings=self._model_config.max_position_embeddings,
+          position_embedding_name='txt_position_embeddings_%d' % i,
+          dropout_prob=self._model_config.hidden_dropout_prob)
+        all_txt_features.append(seq_fea)
+
+        input_mask = tf.map_fn(
+          fn=lambda t: dynamic_mask(t, max_seq_len),
+          elems=tf.to_int32(seq_len))
+        input_masks.append(input_mask)
+
+    return all_txt_features, input_masks
+
+  def image_embeddings(self):
+    if self._img_features is None:
+      return None
+    hidden_size = self._model_config.hidden_size
+    image_features = self._img_features
+    if self._img_emb_size != hidden_size:
+      # Run a linear projection of `hidden_size`
+      image_features = tf.reshape(
+        image_features, shape=[-1, self._img_emb_size])
+      image_features = tf.layers.dense(
+        image_features, hidden_size, name='img_projection')
+    image_features = tf.reshape(
+      image_features, shape=[-1, self._img_feature_num, hidden_size])
+
+    batch_size = tf.shape(image_features)[0]
+    img_fea = multihead_cross_attention.embedding_postprocessor(
+      image_features,
+      use_token_type=self._use_token_type,
+      token_type_ids=tf.zeros(
+        shape=tf.stack([batch_size, self._img_feature_num]), dtype=tf.int32),
+      token_type_vocab_size=self._token_type_vocab_size,
+      reuse_token_type=tf.AUTO_REUSE,
+      use_position_embeddings=self._model_config.use_position_embeddings,
+      max_position_embeddings=self._model_config.max_position_embeddings,
+      position_embedding_name='img_position_embeddings',
+      dropout_prob=self._model_config.hidden_dropout_prob)
+    return img_fea
+
+  def __call__(self, is_training, *args, **kwargs):
+    sub_modules = []
+
+    img_fea = self.image_embeddings()
+    start_token_id = 1 if self._img_feature_num > 0 else 0
+    txt_features, txt_masks = self.text_embeddings(start_token_id)
+
+    if img_fea is not None:
+      batch_size = tf.shape(img_fea)[0]
+    elif txt_features:
+      batch_size = tf.shape(txt_features[0])[0]
+    else:
+      batch_size = None
+
+    hidden_size = self._model_config.hidden_size
+    if batch_size is not None:
+      all_features = []
+      masks = []
+      cls_emb = tf.get_variable(name="cls_emb", shape=[1, 1, hidden_size])
+      cls_emb = tf.tile(cls_emb, [batch_size, 1, 1])
+      all_features.append(cls_emb)
+
+      mask = tf.ones(shape=tf.stack([batch_size, 1]), dtype=tf.int32)
+      masks.append(mask)
+
+      if img_fea is not None:
+        all_features.append(img_fea)
+        mask = tf.ones(shape=tf.stack([batch_size, self._img_feature_num]), dtype=tf.int32)
+        masks.append(mask)
+
+      if txt_features:
+        all_features.extend(txt_features)
+        masks.extend(txt_masks)
+
+      all_fea = tf.concat(all_features, axis=1)
+      input_mask = tf.concat(masks, axis=1)
+      attention_mask = multihead_cross_attention.create_attention_mask_from_input_mask(
+          from_tensor=all_fea, to_mask=input_mask)
+      hidden_act = multihead_cross_attention.get_activation(self._model_config.hidden_act)
+      attention_fea = multihead_cross_attention.transformer_encoder(
+        all_fea,
+        hidden_size=hidden_size,
+        num_hidden_layers=self._model_config.num_hidden_layers,
+        num_attention_heads=self._model_config.num_attention_heads,
+        attention_mask=attention_mask,
+        intermediate_size=self._model_config.intermediate_size,
+        intermediate_act_fn=hidden_act,
+        hidden_dropout_prob=self._model_config.hidden_dropout_prob,
+        attention_probs_dropout_prob=self._model_config.attention_probs_dropout_prob,
+        initializer_range=self._model_config.initializer_range,
+        name='uniter'
+      )  # shape: [batch_size, seq_length, hidden_size]
+      print("attention_fea:", attention_fea.shape)
+      mm_fea = attention_fea[:, 0, :]  # [CLS] feature
+      sub_modules.append(mm_fea)
+
+    if self._other_features is not None:
+      if self._model_config.HasField('other_feature_dnn'):
+        l2_reg = kwargs['l2_reg'] if 'l2_reg' in kwargs else 0
+        other_dnn_layer = dnn.DNN(self._model_config.other_feature_dnn, l2_reg,
+                                  'other_dnn', is_training)
+        other_fea = other_dnn_layer(self._other_features)
+      else:
+        other_fea = self._other_features
+      sub_modules.append(other_fea)
+
+    if len(sub_modules) == 1:
+      return sub_modules[0]
+    output = tf.concat(sub_modules, axis=-1)
+    return output
