160ed9c1705322e2505a8163ca45a654012f485a
lgqfhwy <lgqfhwy@users.noreply.github.com>
2022-08-05 14:21:08 +0800

    [feat]:support only sequence feature && fix neg sampler  bug for sequence feature (#264)
    * fix neg sampler  bug for sequence feature
    * add need_key_feature

--- docs/source/feature/feature.rst
+++ docs/source/feature/feature.rst
@@ -270,6 +270,7 @@ Sequense类特征格式一般为“XX\|XX\|XX”，如用户行为序列特征�
     sequence_features: {
       group_name: "seq_fea"
       allow_key_search: true
+      need_key_feature:true
       seq_att_map: {
         key: "brand"
         key: "cate_id"
@@ -281,6 +282,8 @@ Sequense类特征格式一般为“XX\|XX\|XX”，如用户行为序列特征�
 
 -  sequence_features: 序列特征组的名称
 -  allow_key_search: 当 key 对应的特征没有在 feature_groups 里面时，需要设置为 true, 将会复用对应特征的 embedding.
+-  need_key_feature : 默认为 true, 指过完 target attention 之后的特征会和 key 对应的特征 concat 之后返回。
+   设置为 false 时，将会只返回过完 target attention 之后的特征。
 -  seq_att_map: 具体细节可以参考排序里的 DIN 模型。
 -  NOTE：SequenceFeature一般放在 user 组里面。
 
--- easy_rec/python/layers/input_layer.py
+++ easy_rec/python/layers/input_layer.py
@@ -68,13 +68,14 @@ class InputLayer(object):
   def has_group(self, group_name):
     return group_name in self._feature_groups
 
-  def target_attention(self, dnn_config, deep_fea, name):
+  def target_attention(self, dnn_config, deep_fea, name, need_key_feature=True):
     cur_id, hist_id_col, seq_len = deep_fea['key'], deep_fea[
         'hist_seq_emb'], deep_fea['hist_seq_len']
 
     seq_max_len = tf.shape(hist_id_col)[1]
     emb_dim = hist_id_col.shape[2]
 
+    cur_id = cur_id[:tf.shape(hist_id_col)[0], ...]  # for negative sampler
     cur_ids = tf.tile(cur_id, [1, seq_max_len])
     cur_ids = tf.reshape(cur_ids,
                          tf.shape(hist_id_col))  # (B, seq_max_len, emb_dim)
@@ -96,6 +97,8 @@ class InputLayer(object):
     scores = tf.nn.softmax(scores)  # (B, 1, seq_max_len)
     hist_din_emb = tf.matmul(scores, hist_id_col)  # [B, 1, emb_dim]
     hist_din_emb = tf.reshape(hist_din_emb, [-1, emb_dim])  # [B, emb_dim]
+    if not need_key_feature:
+      return hist_din_emb
     din_output = tf.concat([hist_din_emb, cur_id], axis=1)
     return din_output
 
@@ -108,6 +111,7 @@ class InputLayer(object):
     for seq_att_map_config in all_seq_att_map_config:
       group_name = seq_att_map_config.group_name
       allow_key_search = seq_att_map_config.allow_key_search
+      need_key_feature = seq_att_map_config.need_key_feature
       seq_features = self._seq_input_layer(features, group_name,
                                            feature_name_to_output_tensors,
                                            allow_key_search)
@@ -128,7 +132,10 @@ class InputLayer(object):
         seq_dnn_config.hidden_units.extend([128, 64, 32, 1])
       cur_target_attention_name = 'seq_dnn' + group_name
       seq_fea = self.target_attention(
-          seq_dnn_config, seq_features, name=cur_target_attention_name)
+          seq_dnn_config,
+          seq_features,
+          name=cur_target_attention_name,
+          need_key_feature=need_key_feature)
       all_seq_fea.append(seq_fea)
     # concat all seq_fea
     all_seq_fea = tf.concat(all_seq_fea, axis=1)
