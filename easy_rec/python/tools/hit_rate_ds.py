a25310be4e7bb3a10e2d3ed988d3c40ed71e00d2
wb-lcl910122 <wb-lcl910122@alibaba-inc.com>
2022-08-29 14:45:50 +0800

    add hitrate_hdfs

--- easy_rec/python/tools/hit_rate_ds.py
+++ easy_rec/python/tools/hit_rate_ds.py
@@ -61,138 +61,138 @@ FLAGS = tf.app.flags.FLAGS
 
 
 def compute_hitrate(g, gt_all, hitrate_writer, gt_table=None):
-    """Compute hitrate of each worker.
+  """Compute hitrate of each worker.
 
-    Args:
-      g: a GL Graph instance.
-      gt_reader: reader of input trigger_items_table.
-      hitrate_writer: writer of hitrate table.
-      gt_table: ground truth table.
+  Args:
+    g: a GL Graph instance.
+    gt_reader: reader of input trigger_items_table.
+    hitrate_writer: writer of hitrate table.
+    gt_table: ground truth table.
 
-    Returns:
-      total_hits: total hits of this worker.
-      total_gt_count: total count of ground truth items of this worker.
-    """
-    total_hits = 0.0
-    total_gt_count = 0.0
+  Returns:
+    total_hits: total hits of this worker.
+    total_gt_count: total count of ground truth items of this worker.
+  """
+  total_hits = 0.0
+  total_gt_count = 0.0
 
 
-    for gt_record in gt_all:
-        logging.info("gt_record %s",gt_record)
-        gt_record = list(gt_record)
-        hits, gt_count, src_ids, recall_ids, recall_distances, hitrates, bad_cases, bad_dists = \
-            compute_hitrate_batch(g, gt_record, FLAGS.emb_dim, FLAGS.num_interests, FLAGS.top_k)
-        total_hits += hits
-        total_gt_count += gt_count
+  for gt_record in gt_all:
+    logging.info("gt_record %s",gt_record)
+    gt_record = list(gt_record)
+    hits, gt_count, src_ids, recall_ids, recall_distances, hitrates, bad_cases, bad_dists = \
+        compute_hitrate_batch(g, gt_record, FLAGS.emb_dim, FLAGS.num_interests, FLAGS.top_k)
+    total_hits += hits
+    total_gt_count += gt_count
 
-        src_ids = [str(ids) for ids in src_ids]
-        hitrates = [str(hitrate) for hitrate in hitrates]
-        topk_recalls = [','.join(str(x) for x in ids) for ids in recall_ids]
-        topk_dists = [
-            ','.join('|'.join(str(x)
-                              for x in dist)
-                     for dist in dists)
-            for dists in recall_distances
-        ]
-        bad_cases = [','.join(str(x) for x in bad_case) for bad_case in bad_cases]
-        bad_dists = [','.join(str(x) for x in dist) for dist in bad_dists]
+    src_ids = [str(ids) for ids in src_ids]
+    hitrates = [str(hitrate) for hitrate in hitrates]
+    topk_recalls = [','.join(str(x) for x in ids) for ids in recall_ids]
+    topk_dists = [
+        ','.join('|'.join(str(x)
+                          for x in dist)
+                 for dist in dists)
+        for dists in recall_distances
+    ]
+    bad_cases = [','.join(str(x) for x in bad_case) for bad_case in bad_cases]
+    bad_dists = [','.join(str(x) for x in dist) for dist in bad_dists]
 
-        hitrate_writer.write('\n'.join([
-            '\t'.join(line) for line in zip(src_ids, topk_recalls, topk_dists,
-                                            hitrates, bad_cases, bad_dists)
-        ]))
-    print('total_hits: ', total_hits)
-    print('total_gt_count: ', total_gt_count)
-    return total_hits, total_gt_count
+    hitrate_writer.write('\n'.join([
+        '\t'.join(line) for line in zip(src_ids, topk_recalls, topk_dists,
+                                        hitrates, bad_cases, bad_dists)
+    ]))
+  print('total_hits: ', total_hits)
+  print('total_gt_count: ', total_gt_count)
+  return total_hits, total_gt_count
 
 
 def main():
 
-    tf_config = json.loads(os.environ['TF_CONFIG'])
-    worker_count = len(tf_config['cluster']['worker'])
-    task_index = tf_config['task']['index']
-    job_name = tf_config['task']['type']
+  tf_config = json.loads(os.environ['TF_CONFIG'])
+  worker_count = len(tf_config['cluster']['worker'])
+  task_index = tf_config['task']['index']
+  job_name = tf_config['task']['type']
 
-    hitrate_details_result = FLAGS.hitrate_details_result
-    total_hitrate_result = FLAGS.total_hitrate_result
-    i_emb_table = FLAGS.item_emb_table
-    gt_table = FLAGS.gt_table
-    pipeline_config = config_util.get_configs_from_pipeline_file(
-        FLAGS.pipeline_config_path)
-    logging.info("i_emb_table %s",i_emb_table)
-    logging.info(i_emb_table.startswith('hdfs:'))
-    if not i_emb_table.startswith('hdfs:'):
-        hive_utils = HiveUtils(
-            data_config=pipeline_config.data_config,
-            hive_config=pipeline_config.hive_train_input)
-        i_emb_table = hive_utils.get_table_location(i_emb_table)
-    logging.info('i_emb_table_hdfs_path: %s', i_emb_table)
-    g = load_graph(i_emb_table, FLAGS.emb_dim, FLAGS.knn_metric, FLAGS.timeout,
-                   FLAGS.knn_strict)
-    gl.set_tracker_mode(0)
-    gl.set_field_delimiter('\u0001')
+  hitrate_details_result = FLAGS.hitrate_details_result
+  total_hitrate_result = FLAGS.total_hitrate_result
+  i_emb_table = FLAGS.item_emb_table
+  gt_table = FLAGS.gt_table
+  pipeline_config = config_util.get_configs_from_pipeline_file(
+      FLAGS.pipeline_config_path)
+  logging.info("i_emb_table %s",i_emb_table)
+  logging.info(i_emb_table.startswith('hdfs:'))
+  if not i_emb_table.startswith('hdfs:'):
+    hive_utils = HiveUtils(
+          data_config=pipeline_config.data_config,
+          hive_config=pipeline_config.hive_train_input)
+    i_emb_table = hive_utils.get_table_location(i_emb_table)
+  logging.info('i_emb_table_hdfs_path: %s', i_emb_table)
+  g = load_graph(i_emb_table, FLAGS.emb_dim, FLAGS.knn_metric, FLAGS.timeout,
+                 FLAGS.knn_strict)
+  gl.set_tracker_mode(0)
+  gl.set_field_delimiter('\u0001')
 
-    cluster = tf.train.ClusterSpec({
-        'ps': tf_config['cluster']['ps'],
-        'worker': tf_config['cluster']['worker']
-    })
-    server = tf.train.Server(cluster, job_name=job_name, task_index=task_index)
+  cluster = tf.train.ClusterSpec({
+      'ps': tf_config['cluster']['ps'],
+      'worker': tf_config['cluster']['worker']
+  })
+  server = tf.train.Server(cluster, job_name=job_name, task_index=task_index)
 
-    if job_name == 'ps':
-        server.join()
+  if job_name == 'ps':
+    server.join()
+  else:
+    worker_hosts = [
+        str(host.split(':')[0]) + ':888' + str(i)
+        for i, host in enumerate(tf_config['cluster']['worker'])
+    ]
+    worker_hosts = ','.join(worker_hosts)
+    g.init(task_index=task_index, task_count=worker_count, hosts=worker_hosts)
+    # Your model, use g to do some operation, such as sampling
+    if gt_table.startswith('hdfs:'):
+      gt_file = tf.gfile.GFile(gt_table,'r')
+      list1 , gt_all = [] , []
+      i = 0
+      for gt in gt_file:
+        i+=1
+        list2 = gt.strip().split('\t')
+        list2[0] ,list2[3] = int(list2[0]) , int(list2[3])
+        list1.append(tuple(i for i in list2))
+        if i >= FLAGS.batch_size:
+          gt_all.append(list1)
+          list1 , i = [] , 0
+      if i !=0 :
+        gt_all.append(list1)
     else:
-        worker_hosts = [
-            str(host.split(':')[0]) + ':888' + str(i)
-            for i, host in enumerate(tf_config['cluster']['worker'])
-        ]
-        worker_hosts = ','.join(worker_hosts)
-        g.init(task_index=task_index, task_count=worker_count, hosts=worker_hosts)
-        # Your model, use g to do some operation, such as sampling
-        if gt_table.startswith('hdfs:'):
-            gt_file = tf.gfile.GFile(gt_table,'r')
-            list1 , gt_all = [] , []
-            i = 0
-            for gt in gt_file:
-                i+=1
-                list2 = gt.strip().split('\t')
-                list2[0] ,list2[3] = int(list2[0]) , int(list2[3])
-                list1.append(tuple(i for i in list2))
-                if i >= FLAGS.batch_size:
-                    gt_all.append(list1)
-                    list1 , i = [] , 0
-            if i !=0 :
-                gt_all.append(list1)
-        else:
-            gt_reader = HiveUtils(
-                data_config=pipeline_config.data_config,
-                hive_config=pipeline_config.hive_train_input,
-                selected_cols='*')
-            gt_all = gt_reader.hive_read_lines(gt_table, FLAGS.batch_size)
-        if tf.gfile.IsDirectory(hitrate_details_result):
-            tf.gfile.MakeDirs(hitrate_details_result)
-        hitrate_details_result = os.path.join(hitrate_details_result,
-                                              'part-%s' % task_index)
-        details_writer = tf.gfile.GFile(hitrate_details_result, 'w')
-        print('Start compute hitrate...')
-        total_hits, total_gt_count = compute_hitrate(g, gt_all, details_writer,
-                                                     gt_table)
-        var_total_hitrate, var_worker_count = reduce_hitrate(
-            cluster, total_hits, total_gt_count, task_index)
+      gt_reader = HiveUtils(
+          data_config=pipeline_config.data_config,
+          hive_config=pipeline_config.hive_train_input,
+          selected_cols='*')
+      gt_all = gt_reader.hive_read_lines(gt_table, FLAGS.batch_size)
+    if tf.gfile.IsDirectory(hitrate_details_result):
+      tf.gfile.MakeDirs(hitrate_details_result)
+    hitrate_details_result = os.path.join(hitrate_details_result,
+                                          'part-%s' % task_index)
+    details_writer = tf.gfile.GFile(hitrate_details_result, 'w')
+    print('Start compute hitrate...')
+    total_hits, total_gt_count = compute_hitrate(g, gt_all, details_writer,
+                                                 gt_table)
+    var_total_hitrate, var_worker_count = reduce_hitrate(
+        cluster, total_hits, total_gt_count, task_index)
 
-        with tf.train.MonitoredTrainingSession(
-                master=server.target, is_chief=(task_index == 0)) as sess:
-            outs = sess.run([var_total_hitrate, var_worker_count])
+    with tf.train.MonitoredTrainingSession(
+        master=server.target, is_chief=(task_index == 0)) as sess:
+      outs = sess.run([var_total_hitrate, var_worker_count])
 
-        # write after all workers have completed the calculation of hitrate.
-        print('outs: ', outs)
-        if outs[1] == worker_count:
-            logging.info(outs)
-            with tf.gfile.GFile(total_hitrate_result, 'w') as total_writer:
-                total_writer.write(str(outs[0]))
+    # write after all workers have completed the calculation of hitrate.
+    print('outs: ', outs)
+    if outs[1] == worker_count:
+      logging.info(outs)
+      with tf.gfile.GFile(total_hitrate_result, 'w') as total_writer:
+        total_writer.write(str(outs[0]))
 
-        details_writer.close()
-        g.close()
-        print('Compute hitrate done.')
+    details_writer.close()
+    g.close()
+    print('Compute hitrate done.')
 
 
 if __name__ == '__main__':
