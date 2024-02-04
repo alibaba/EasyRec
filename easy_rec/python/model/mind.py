0058ec62bcd216abf23616ff90198c4c22ee0ce0
tiankongdeguiji <hongsheng.jhs@alibaba-inc.com>
2024-02-04 09:37:49 +0800

    [feat]: add user embedding and item embedding outputs for match model (#453)

--- easy_rec/python/model/mind.py
+++ easy_rec/python/model/mind.py
@@ -423,14 +423,23 @@ class MIND(MatchModel):
 
   def get_outputs(self):
     if self._loss_type == LossType.CLASSIFICATION:
-      return ['logits', 'probs', 'user_emb', 'item_emb', 'user_emb_num']
+      return [
+          'logits', 'probs', 'user_emb', 'item_emb', 'user_emb_num',
+          'user_interests', 'item_tower_emb'
+      ]
     elif self._loss_type == LossType.SOFTMAX_CROSS_ENTROPY:
       self._prediction_dict['logits'] = tf.squeeze(
           self._prediction_dict['logits'], axis=-1)
       self._prediction_dict['probs'] = tf.nn.sigmoid(
           self._prediction_dict['logits'])
-      return ['logits', 'probs', 'user_emb', 'item_emb', 'user_emb_num']
+      return [
+          'logits', 'probs', 'user_emb', 'item_emb', 'user_emb_num',
+          'user_interests', 'item_tower_emb'
+      ]
     elif self._loss_type == LossType.L2_LOSS:
-      return ['y', 'user_emb', 'item_emb', 'user_emb_num']
+      return [
+          'y', 'user_emb', 'item_emb', 'user_emb_num', 'user_interests',
+          'item_tower_emb'
+      ]
     else:
       raise ValueError('invalid loss type: %s' % str(self._loss_type))
