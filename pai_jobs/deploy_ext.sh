4d3cafbc9b5c05412e615951e27d9b5904b84b85
卫苏 <weisu.yxd@alibaba-inc.com>
2024-11-13 11:20:57 +0800

    add tensorflow-probability to resource tar

--- pai_jobs/deploy_ext.sh
+++ pai_jobs/deploy_ext.sh
@@ -100,7 +100,6 @@ fi
 cp -R $root_dir/easy_rec ./easy_rec
 sed -i -e "s/\[VERSION\]/$VERSION/g" easy_rec/__init__.py
 find -L easy_rec -name "*.pyc" | xargs rm -rf
-echo "tensorflow-probability==0.5.0" > requirements.txt
 
 if [ ! -d "datahub" ]
 then
@@ -144,7 +143,23 @@ then
   rm -rf faiss.tar.gz
 fi
 
-tar -cvzhf $RES_PATH easy_rec datahub lz4 cprotobuf kafka faiss run.py requirements.txt
+if [ ! -d "tensorflow_probability" ]
+then
+  if [ ! -e "tensorflow_probability" ]
+  then
+    wget http://easyrec.oss-cn-beijing.aliyuncs.com/3rdparty/probability-0.5.0.tar.gz
+    if [ $? -ne 0 ]
+    then
+      echo "tensorflow_probability download failed."
+    fi
+  fi
+  tar -xzvf probability-0.5.0.tar.gz --strip-components=1 probability-0.5.0/tensorflow_probability
+  rm -rf tensorflow_probability/examples
+  rm -rf tensorflow_probability/g3doc
+  rm -rf probability-0.5.0.tar.gz
+fi
+
+tar -cvzhf $RES_PATH easy_rec datahub lz4 cprotobuf kafka faiss tensorflow_probability run.py
 
 # 2 means generate only
 if [ $mode -ne 2 ]
