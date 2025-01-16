8e2def1b93a7bac598eb44b9a211cb49c325bbce
yanzhen1233 <wb-yz775491@alibaba-inc.com>
2025-01-16 18:37:38 +0800

    Build docker (#515)
    * Updated Docker image

---
+++ scripts/build_docker_tf115.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+bash scripts/gen_proto.sh
+if [ $? -ne 0 ]
+then
+  echo "gen proto failed"
+  exit 1
+fi
+
+version=`grep "__version__" easy_rec/version.py | awk '{ if($1 == "__version__") print $NF}'`
+# strip "'"
+version=${version//\'/}
+echo "EasyRec Version: $version"
+
+if [ -z "$version" ]
+then
+  echo "Failed to get EasyRec version"
+  exit 1
+fi
+
+sudo docker build --network=host . -f docker/Dockerfile_tf115 -t  mybigpai-public-registry.cn-beijing.cr.aliyuncs.com/easyrec/easyrec:py36-tf1.15-${version}
