d6bd2b6d42739124bc822f9383175719be01c838
yangxudong <weisu.yxd@alibaba-inc.com>
2025-05-29 18:49:23 +0800

    fix bug of gfile compatibility

--- easy_rec/python/test/train_eval_test.py
+++ easy_rec/python/test/train_eval_test.py
@@ -12,6 +12,7 @@ import numpy as np
 import six
 import tensorflow as tf
 from distutils.version import LooseVersion
+from tensorflow.python.platform import gfile
 
 from easy_rec.python.main import predict
 from easy_rec.python.utils import config_util
